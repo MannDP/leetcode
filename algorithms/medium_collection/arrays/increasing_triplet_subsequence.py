d86fa7df14d7b777c40a873d5290562c91564fa1
MannDP <mann.patel@datadoghq.com>
2022-03-24 21:58:59 -0400

    increasing triplet subsequence

---
+++ algorithms/medium_collection/arrays/increasing_triplet_subsequence.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float('inf')
+        for num in nums:
+            if num > second:
+                return True
+            elif num > first:
+                second = min(second, num)
+            else:
+                first = num
+        return False
+    
+s = Solution()
+print(s.increasing_triplet([3, 1, 2]))
